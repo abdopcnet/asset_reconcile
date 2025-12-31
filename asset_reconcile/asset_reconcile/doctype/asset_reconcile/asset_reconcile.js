0109fc27c2b65e5ed45a27d797d63bc466130f1f
abdopcnet <abdopcnet@gmail.com>
2025-12-31 02:16:12 +0000

    🔄 asset_reconcile.js

--- asset_reconcile/asset_reconcile/doctype/asset_reconcile/asset_reconcile.js
+++ asset_reconcile/asset_reconcile/doctype/asset_reconcile/asset_reconcile.js
@@ -14,31 +14,38 @@ frappe.ui.form.on("Asset Reconcile", {
 			prompt_qty: false,
 		});
 
+		// Override scan_api_call to pass location parameter
+		frm.barcode_scanner.scan_api_call = function (input, callback) {
+			frappe
+				.call({
+					method: this.scan_api,
+					args: {
+						search_value: input,
+						company: this.frm.doc.company,
+						location: this.frm.doc.location,
+					},
+				})
+				.then((r) => {
+					callback(r);
+				})
+				.catch((err) => {
+					callback({ message: {} });
+				});
+		};
+
 		// Override update_table to work with Assets
 		frm.barcode_scanner.update_table = function (data) {
 			return new Promise((resolve, reject) => {
 				let cur_grid = this.frm.fields_dict[this.items_table_name].grid;
 				frappe.flags.trigger_from_barcode_scanner = true;
 
-				const {
-					asset,
-					asset_name,
-					location,
-					value_after_depreciation,
-					gross_purchase_amount,
-					custodian,
-					status,
-					asset_category,
-					department,
-					cost_center,
-					item_code,
-				} = data;
+				const { asset, asset_name, location, asset_category, item_code } = data;
 
 				if (!asset) {
 					this.show_alert(__("Cannot find Asset with this Barcode"), "red");
 					this.clean_up();
 					this.play_fail_sound();
-					reject();
+					reject(new Error("Asset not found"));
 					return;
 				}
 
@@ -64,11 +71,12 @@ frappe.ui.form.on("Asset Reconcile", {
 					return;
 				}
 
-				// Add new row
+				// Add new row at the beginning (idx = 1)
 				let row = frappe.model.add_child(
 					this.frm.doc,
 					cur_grid.doctype,
-					this.items_table_name
+					this.items_table_name,
+					1
 				);
 				this.frm.script_manager.trigger(
 					`${this.items_table_name}_add`,
@@ -80,14 +88,8 @@ frappe.ui.form.on("Asset Reconcile", {
 					asset: asset,
 					asset_name: asset_name,
 					location: location,
-					custodian: custodian,
-					status: status,
 					asset_category: asset_category,
-					department: department,
-					cost_center: cost_center,
 					item_code: item_code,
-					value_after_depreciation: value_after_depreciation || gross_purchase_amount,
-					gross_purchase_amount: gross_purchase_amount,
 					system_quantity: 1,
 					physical_count: 1,
 					variance: 0,
@@ -109,9 +111,12 @@ frappe.ui.form.on("Asset Reconcile", {
 		if (!frm.doc.reconciliation_time) {
 			frm.set_value("reconciliation_time", frappe.datetime.now_time());
 		}
-		if (!frm.doc.counted_by) {
-			frm.set_value("counted_by", frappe.session.user);
-		}
+		frm.set_value("counted_by", frappe.session.user);
+		frm.set_df_property("counted_by", "read_only", 1);
+	},
+
+	before_save(frm) {
+		frm.set_value("counted_by", frappe.session.user);
 	},
 
 	scan_barcode(frm) {
@@ -120,16 +125,11 @@ frappe.ui.form.on("Asset Reconcile", {
 		}
 	},
 
-	scan_mode(frm) {
-		if (frm.doc.scan_mode) {
-			frappe.show_alert({
-				message: __("Scan mode enabled. Scan barcode to add assets."),
-				indicator: "green",
-			});
-		}
-	},
-
 	refresh(frm) {
+		if (!frm.doc.counted_by) {
+			frm.set_value("counted_by", frappe.session.user);
+		}
+		frm.set_df_property("counted_by", "read_only", 1);
 		if (frm.doc.docstatus === 0) {
 			frm.add_custom_button(__("Fetch Assets from Location"), function () {
 				frm.events.get_assets_from_location(frm);
@@ -157,17 +157,10 @@ frappe.ui.form.on("Asset Reconcile", {
 						row.asset = asset.name;
 						row.asset_name = asset.asset_name;
 						row.location = asset.location;
-						row.custodian = asset.custodian;
-						row.status = asset.status;
 						row.asset_category = asset.asset_category;
-						row.department = asset.department;
-						row.cost_center = asset.cost_center;
 						row.item_code = asset.item_code;
 						row.system_quantity = 1;
 						row.physical_count = 1;
-						row.gross_purchase_amount = asset.gross_purchase_amount;
-						row.value_after_depreciation =
-							asset.value_after_depreciation || asset.gross_purchase_amount;
 					});
 					frm.refresh_field("assets");
 					frm.trigger("calculate_totals");
@@ -213,18 +206,10 @@ frappe.ui.form.on("Asset Reconcile Item", {
 						frappe.model.set_value(cdt, cdn, {
 							asset_name: asset.asset_name,
 							location: asset.location,
-							custodian: asset.custodian,
-							status: asset.status,
 							asset_category: asset.asset_category,
-							department: asset.department,
-							cost_center: asset.cost_center,
 							item_code: asset.item_code,
-							item_name: asset.item_name,
 							system_quantity: asset.asset_quantity || 1,
 							physical_count: asset.asset_quantity || 1,
-							gross_purchase_amount: asset.gross_purchase_amount,
-							value_after_depreciation:
-								asset.value_after_depreciation || asset.gross_purchase_amount,
 						});
 						frm.trigger("calculate_totals");
 					}
@@ -248,9 +233,6 @@ frappe.ui.form.on("Asset Reconcile Item", {
 
 	assets_add(frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
-		if (frm.doc.set_location && !row.location) {
-			frappe.model.set_value(cdt, cdn, "location", frm.doc.set_location);
-		}
 		if (!row.system_quantity) {
 			frappe.model.set_value(cdt, cdn, "system_quantity", 1);
 		}
