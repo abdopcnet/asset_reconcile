a330cc701c2f13518d684f7e0f98318bfa4a9920
abdopcnet <abdopcnet@gmail.com>
2026-01-07 01:18:07 +0000

    01-18-07-01-2026

---
+++ asset_reconcile/asset_reconcile/doctype/asset_reconcile_item/asset_reconcile_item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, abdopcnet@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Asset Reconcile Item', {
+	// refresh(frm) {
+	// 	// Logic moved to parent doctype JS for better context handling
+	// }
+});
