4c7e02ef07b17c762c9e4c08613250a5e36cb084
abdopcnet <abdopcnet@gmail.com>
2026-01-07 01:18:07 +0000

    01-18-07-01-2026

--- asset_reconcile/asset_reconcile/doctype/asset_reconcile_item/asset_reconcile_item.py
+++ asset_reconcile/asset_reconcile/doctype/asset_reconcile_item/asset_reconcile_item.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2025, abdopcnet@gmail.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class AssetReconcileItem(Document):
-	pass
+    """
+    Document controller for Asset Reconcile Item child table.
+    Logic is handled in the parent DocType (Asset Reconcile).
+    """
+    pass
